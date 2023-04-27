fa3bcdcc0ec0f0360dba295cc912fb72ff2346aa
suthyscott <sutherlandscott21@gmail.com>
2023-04-27 10:02:38 -0600

    breaking down state

--- src/App.jsx
+++ src/App.jsx
@@ -3,44 +3,73 @@ import ListItem from "./ListItem"
 import { useState } from "react"
 
 const App = () => {
-    const [itemList, setItemList] = useState(["change oil","clean truck","sweep garage","wash motorcycle","organize", 'clean my room', 'beat kyle at ping pong'])
+    const [itemList, setItemList] = useState([
+        "change oil",
+        "clean truck",
+        "sweep garage",
+        "wash motorcycle",
+        "organize",
+        "clean my room",
+        "beat kyle at ping pong"
+    ])
+    const [inputVal, setInputVal] = useState('')
 
-    console.log(itemList)
+    console.log(inputVal)
 
-    // Rendering a dynamic state value using a for loop + variable
-    // const itemsDisplay = []
-    // for(let i = 0; i < itemList.length; i++){
-    //     itemsDisplay.push(<ListItem item={itemList[i]} />)
-    // }
-    // console.log(itemsDisplay)
-
-
-    // Rendering a dynamic state value using a .map + variable
-    // let itemsDisplay = itemList.map((itemValue, index) => {
-    //     return <ListItem item={itemValue} key={index}/>
-    // })
     return (
         <div className="App">
-            This is app.js
-            {/* Displaying state values with hard coded values */}
-            {/* <ListItem item={itemList[0]} />
-            <ListItem item={itemList[1]} />
-            <ListItem item={itemList[2]} />
-            <ListItem item={itemList[3]} />
-            <ListItem item={itemList[4]} /> */}
+            <h1>To Do List</h1>
 
-            {/* Displaying state values inline with .map */}
-            {/* {itemList.map((itemValue, index) => {
-                return <ListItem item={itemValue} key={index}/>
-            })} */}
-
+            <form>
+                <input placeholder="Write your new to do item here" onChange={e => setInputVal(e.target.value)}/>
+                InputVal: {inputVal}
+                <button>Add</button>
+            </form>
             {/* Displaying state values inline w/implicit return thru arrow function*/}
-            {itemList.map((itemValue, index) => <ListItem item={itemValue} key={index}/>)}
-
-            {/* the display variable from the for loop or .map above the return */}
-            {/* {itemsDisplay} */}
+            {itemList.map((itemValue, index) => (
+                <ListItem item={itemValue} key={index} />
+            ))}
         </div>
     )
 }
 
 export default App
+
+// Rendering a dynamic state value using a for loop + variable
+// const itemsDisplay = []
+// for(let i = 0; i < itemList.length; i++){
+//     itemsDisplay.push(<ListItem item={itemList[i]} />)
+// }
+// console.log(itemsDisplay)
+
+// Rendering a dynamic state value using a .map + variable
+// let itemsDisplay = itemList.map((itemValue, index) => {
+//     return <ListItem item={itemValue} key={index}/>
+// })
+
+{
+    /* Displaying state values with hard coded values */
+}
+{
+    /* <ListItem item={itemList[0]} />
+            <ListItem item={itemList[1]} />
+            <ListItem item={itemList[2]} />
+            <ListItem item={itemList[3]} />
+        <ListItem item={itemList[4]} /> */
+}
+
+{
+    /* Displaying state values inline with .map */
+}
+{
+    /* {itemList.map((itemValue, index) => {
+                return <ListItem item={itemValue} key={index}/>
+            })} */
+}
+
+{
+    /* the display variable from the for loop or .map above the return */
+}
+{
+    /* {itemsDisplay} */
+}
